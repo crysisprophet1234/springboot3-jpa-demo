b8012b8fe50db78e29af9007a0e6518c936b9f93
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-02-07 00:34:26 -0300

    first commit

---
+++ src/main/java/com/poletto/cursospringjava/CursospringjavaApplication.java
@@ -0,0 +1,13 @@
+package com.poletto.cursospringjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poletto/cursospringjava/CursospringjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poletto.cursospringjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
