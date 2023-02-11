908823f5c9b8b91afb3b665d3f1a174937b09e94
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-02-11 20:14:10 -0300

    Added findByName default method on jpaRepository < User >

--- src/main/java/com/poletto/cursospringjava/repos/UserRepository.java
+++ src/main/java/com/poletto/cursospringjava/repos/UserRepository.java
@@ -1,9 +1,13 @@
 package com.poletto.cursospringjava.repos;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.poletto.cursospringjava.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+	public Optional<User> findByName(String name);
+	
 }
