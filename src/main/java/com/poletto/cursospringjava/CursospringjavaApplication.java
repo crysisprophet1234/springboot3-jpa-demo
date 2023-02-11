06cda6b77a55e5db11c24029ac756428476ce383
Leonardo Poletto Casagrande <leocasagrande1234@hotmail.com>
2023-02-11 20:10:05 -0300

    Added spring security dependency, no authorization needed

--- src/main/java/com/poletto/cursospringjava/CursospringjavaApplication.java
+++ src/main/java/com/poletto/cursospringjava/CursospringjavaApplication.java
@@ -2,12 +2,14 @@ package com.poletto.cursospringjava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication (exclude = {SecurityAutoConfiguration.class})
 public class CursospringjavaApplication {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(CursospringjavaApplication.class, args);
+		SpringApplication.run(CursospringjavaApplication.class, args);			
 	}
-
+	
 }
